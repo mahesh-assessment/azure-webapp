a1ebde9c454f991447a5eb87bb8135c814a6b3dc
root <root@HPE-5CG2184QNH.localdomain>
2026-01-20 22:01:58 +0530

    Adding storage account for sql audit

--- Infra/terraform/sql.tf
+++ Infra/terraform/sql.tf
@@ -9,25 +9,15 @@ resource "azurerm_mssql_server" "sql" {
   administrator_login          = "sqladminuser"
   administrator_login_password = data.azurerm_key_vault_secret.sql_admin_password.value
 
-  # Disable public network access (PII requirement)
   public_network_access_enabled = false
 
-  # Enable system-assigned managed identity
   identity {
     type = "SystemAssigned"
   }
 
-  # Azure AD admin for AAD authentication
   azuread_administrator {
     login_username = "aad-sql-admin"
     object_id      = data.azurerm_client_config.current.object_id
-  }
-
-  # SQL Auditing (AzureRM v4 – inline configuration)
-  auditing {
-    storage_endpoint           = azurerm_storage_account.audit.primary_blob_endpoint
-    storage_account_access_key = azurerm_storage_account.audit.primary_access_key
-    retention_in_days          = 90
   }
 }
 
@@ -36,18 +26,6 @@ resource "azurerm_mssql_database" "db" {
   server_id = azurerm_mssql_server.sql.id
   sku_name  = "Basic"
 
-  # Azure SQL has TDE enabled by default (AES-256)
-  # No explicit Terraform resource required
-
-  long_term_retention_policy {
-    weekly_retention  = "P0D"
-    monthly_retention = "P0D"
-    yearly_retention  = "P0D"
-  }
-
-  timeouts {
-    create = "30m"
-    read   = "10m"
-  }
+  # TDE is enabled by default (AES-256)
 }
 
---
+++ Infra/terraform/sql_auditing.tf
@@ -0,0 +1,7 @@
+resource "azurerm_mssql_server_auditing_policy" "audit" {
+  server_id                  = azurerm_mssql_server.sql.id
+  storage_endpoint           = azurerm_storage_account.audit.primary_blob_endpoint
+  storage_account_access_key = azurerm_storage_account.audit.primary_access_key
+  retention_in_days          = 90
+}
+
